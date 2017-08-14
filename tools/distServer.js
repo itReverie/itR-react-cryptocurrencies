c9fe6bd3cc34f1c830fd3f0ca42a06285a9d0f6b
itreverie <brendadhk@gmail.com>
2017-08-14 17:35:32 -0400

    Adding a correct configuration for a port

--- tools/distServer.js
+++ tools/distServer.js
@@ -5,7 +5,7 @@ import compression from 'compression';
 
 /*eslint-disable no-console */
 
-const port = 5000;
+const port = process.env.port || 3000;
 const app = express();
 
 app.use(compression());
--- tools/srcServer.js
+++ tools/srcServer.js
@@ -6,7 +6,7 @@ import open from 'open';
 
 /* eslint-disable no-console */
 
-const port = 5000;
+const port = process.env.port || 3000;
 const app = express();
 const compiler = webpack(config);
 
