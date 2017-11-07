6a463f35266e69a3ad288ad0e01a93475d941ce6
itreverie <brendadhk@gmail.com>
2017-11-07 11:52:02 -0500

    Adding prop-types in the package.json

--- src/components/common/NumberInput.js
+++ src/components/common/NumberInput.js
@@ -1,6 +1,6 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import NumericInput from 'react-numeric-input';
-
+import PropTypes from 'prop-types';
 
 const NumberInput = ({name,  onChange, placeholder, value, error}) => {
   let wrapperClass = 'form-group';
