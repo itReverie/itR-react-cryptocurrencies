c226d65194801e98248ce55de44a6af6672ad766
itreverie <brendadhk@gmail.com>
2017-11-11 12:08:59 -0500

    adding a webpack to production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,154 +1,159 @@
+// For info about this file refer to webpack and webpack-hot-middleware documentation
+// For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
 import webpack from 'webpack';
-import path from 'path';
-import eslintFormatter from 'react-dev-utils/eslintFormatter';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
 
-const GLOBALS ={
-  'process.env.PORT' : JSON.stringify('5000')
-};
-
-const customLoaderOptions = {
-  devServer: {
-    contentBase: path.resolve(__dirname, './dist')
-  },
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production'),
+  __DEV__: false
 };
 
 export default {
-  devtool: 'source-map', //inline-source-map
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  devtool: 'source-map', // more info:https://webpack.js.org/guides/production/#source-mapping and https://webpack.js.org/configuration/devtool/
   entry: path.resolve(__dirname, 'src/index'),
   target: 'web',
   output: {
-    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',            // In development there are not real physyical files. This is just to let the browser know it need sot create in memory thise files
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].js'
   },
   plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.DefinePlugin(GLOBALS),
-    new ExtractTextPlugin('styles.css'),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
-    new webpack.LoaderOptionsPlugin({options: customLoaderOptions})
+    // Hash the files using MD5 so that their names change when the content changes.
+    new WebpackMd5Hash(),
 
+    // Tells React to build in prod mode. https://facebook.github.io/react/downloads.html
+    new webpack.DefinePlugin(GLOBALS),
+
+    // Generate an external css file with a hash in the filename
+    new ExtractTextPlugin('[name].[contenthash].css'),
+
+    // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
+    new HtmlWebpackPlugin({
+      favicon: 'src/favicon.ico',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
+
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
   ],
   module: {
-    strictExportPresence: true,
     rules: [
-      // TODO: Disable require.ensure as it's not a standard language feature.
-      // We are waiting for https://github.com/facebookincubator/create-react-app/issues/2176.
-      // { parser: { requireEnsure: false } },
-
-      // First, run the linter.
-      // It's important to do this before Babel processes the JS.
       {
-        test: /\.(js|jsx)$/,
-        enforce: 'pre',
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.eot(\?v=\d+.\d+.\d+)?$/,
         use: [
           {
+            loader: 'url-loader',
             options: {
-              formatter: eslintFormatter,
-
-            },
-            loader: require.resolve('eslint-loader'),
-          },
-        ],
-        include: __dirname + '/src/'
-      },
-      // ** ADDING/UPDATING LOADERS **
-      // The "file" loader handles all assets unless explicitly excluded.
-      // The `exclude` list *must* be updated with every change to loader extensions.
-      // When adding a new loader, you must add its `test`
-      // as a new entry in the `exclude` list for "file" loader.
-
-      // "file" loader makes sure those assets get served by WebpackDevServer.
-      // When you `import` an asset, you get its (virtual) filename.
-      // In production, they would get copied to the `build` folder.
-      {
-        exclude: [
-          /\.html$/,
-          /\.(js|jsx)$/,
-          /\.css$/,
-          /\.json$/,
-          /\.bmp$/,
-          /\.gif$/,
-          /\.jpe?g$/,
-          /\.png$/,
-          /\.scss$/,
-          /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-          /\.(woff|woff2)$/,
-          /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-          /\.svg(\?v=\d+\.\d+\.\d+)?$/
-        ],
-        loader: require.resolve('file-loader'),
-        options: {
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-      },
-      // "url" loader works like "file" loader except that it embeds assets
-      // smaller than specified limit in bytes as data URLs to avoid requests.
-      // A missing `test` is equivalent to a match.
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-      },
-      // Process JS with Babel.
-      {
-        test: /\.(js|jsx)$/,
-        include: __dirname + '/src/',
-        loader: require.resolve('babel-loader'),
-        options: {
-
-          // This is a feature of `babel-loader` for webpack (not Babel itself).
-          // It enables caching results in ./node_modules/.cache/babel-loader/
-          // directory for faster rebuilds.
-          cacheDirectory: true,
-        },
+              name: '[name].[ext]'
+            }
+          }
+        ]
       },
       {
-        test: /\.s?css$/,
-        include: __dirname + '/src/',
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
-
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'application/font-woff',
+              name: '[name].[ext]'
+            }
+          }
+        ]
       },
       {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        include: __dirname + '/node_modules/bootstrap-sass/',
-        loader: require.resolve('file-loader')
-        //loader: 'file-loader'},
-      },
-      {
-        test: /\.(woff|woff2)$/,
-        include: __dirname + '/node_modules/bootstrap-sass/',
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 5000,
-          name: 'static/media/[name].[hash:8].[ext]'
-          //?name=__media/[path][name][hash].[ext]
-          //prefix: 'font/'
-        }
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        include: __dirname + '/node_modules/bootstrap-sass/',
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
-          mimetype : 'application/octet-stream',
-        }
+        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'application/octet-stream',
+              name: '[name].[ext]'
+            }
+          }
+        ]
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        include: __dirname + '/node_modules/bootstrap-sass/',
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
-          mimetype : 'application/octet-stream',
-        }
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              mimetype: 'image/svg+xml',
+              name: '[name].[ext]'
+            }
+          }
+        ]
       },
-    ],
-  },
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        exclude: /node_modules/,
+        include: __dirname + '/src/',
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                include: path.resolve(__dirname, 'src'),
+                modules: true,
+                minimize: true,
+                sourceMap: true
+              }
+            }, {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [
+                  require('autoprefixer')
+                ],
+                sourceMap: true
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                includePaths: [path.resolve(__dirname, 'src', 'scss')],
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }
+    ]
+  }
 };
