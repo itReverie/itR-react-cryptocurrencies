21f2bf817198b5845571e43a396600a1c792f386
itReverie <brenda@itreverie.com>
2018-05-07 15:43:56 -0400

    editing webpack prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,14 +1,6 @@
-// For info about this file refer to webpack and webpack-hot-middleware documentation
-// For info on how we're generating bundles with hashed filenames for cache busting: https://medium.com/@okonetchnikov/long-term-caching-of-static-assets-with-webpack-1ecb139adb95#.w99i89nsz
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
-
-const GLOBALS = {
-  'process.env.NODE_ENV': JSON.stringify('production'),
-  'process.env.PORT' : JSON.stringify('5000'),
-  __DEV__: false
-};
 
 export default {
   resolve: {
